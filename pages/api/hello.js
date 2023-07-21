6ac258954c66ce329731dccc65a86779edbbda49
Takumi Abe <start.7185.herausforderung@gmail.com>
2023-07-21 14:20:11 +0900

    wip

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,8 @@ export const config = {
   runtime: 'edge',
 }
 
-export default async function getServerSideProps() {
-  const random = Math.floor(Math.random() * 100);
-  return new Response(random);
+export default async function (req) {
+  console.log("@@@ projcess is ", process);
+  const kvvalue = await process.env.KV.get('test')
+  return new Response(kvvalue)
 }
